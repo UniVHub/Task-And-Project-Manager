c849f864edffab5301602cd2378fd24015a0059b
Carlos Hernandez <carlos.hernandez.agudelo@correounivalle.edu.co>
2024-07-04 20:16:38 -0500

    chore: Update file extensions in backend/stop.sh

--- backend/stop.sh
+++ backend/stop.sh
@@ -2,11 +2,11 @@
 
 
 declare -a files=(
-	"config_map.yaml"
-	"PostgreSQL.yaml"
-	"PostgreSQL-storage.yaml"
-	"task_manager-backend.yaml"
-	"task_manager-storage.yaml"
+	"config_map.yml"
+	"PostgreSQL-storage.yml"
+	"PostgreSQL.yml"
+	"task_manager-storage.yml"
+	"task_manager-backend.yml"
 )
 
 for file in "${files[@]}"; do
